594439ca9e42a13d485f401bcca522d5d973c97b
kalebe <nicolaskalebe223@gmail.com>
2025-07-18 10:29:59 -0300

    retirado py

--- py/E.py
+++
@@ -1,7 +0,0 @@
-from math import gcd
-entrada = map(int, input().split(" ")) 
-Y, K = entrada
-X = 1
-for i in range(K):
-    X += gcd(X, Y)
-print(X)
--- py/L.py
+++
@@ -1,3 +0,0 @@
-from math import log, ceil
-qubits = log(int(input()) * 1000000 * 8,2)
-print(f"{ceil(qubits)}")
